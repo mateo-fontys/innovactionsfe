53bef3957986ee19c1321907a6295319df12fca5
Andreiana Matei-Octavian <matei.andreiana@logichainge.com>
2025-03-24 11:26:49 +0200

    starting implementation.

---
+++ src/app/pages/creator/taskCreation/taskCreation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creation',
+  standalone: true,
+  templateUrl: './taskCreation.component.html',
+  styleUrls: ['./taskCreation.component.css']
+})
+export class TaskCreationComponent {
+  // Component logic here
+}
