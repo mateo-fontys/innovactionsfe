5e51b5a93deddd002e1e8045bba7a4a29e80cb31
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-06-05 14:04:33 +0200

    added experience points after a bug report is approved and also removed the mock data of the leaderboard with emili

--- src/app/leaderboards/all-time-leaderboard/all-time-leaderboard.component.ts
+++ src/app/leaderboards/all-time-leaderboard/all-time-leaderboard.component.ts
@@ -14,17 +14,6 @@ export class AllTimeLeaderboardComponent implements OnInit {
 
   async ngOnInit() {
     const response = await LeaderboardService.getAllTimeLeaderboard();
-    console.log(response)
     this.leaderboard = response.leaderboard
-
-    // if (!this.leaderboard || this.leaderboard.length === 0) {
-    //   this.leaderboard = Array.from({ length: 10 }, (_, i) => ({
-    //     id: i + 1,
-    //     username: `MockUser${i + 1}`,
-    //     virtualMoney: Math.floor(Math.random() * 10000) + 1000,
-    //     experiencePoints: Math.floor(Math.random() * 5000) + 500,
-    //   }));
-    //   this.leaderboard.sort((a, b) => b.experiencePoints - a.experiencePoints);
-    // }
   }
 }
