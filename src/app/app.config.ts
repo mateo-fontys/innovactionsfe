c6dec9ce2fce987dabc4f1311a6c5ed61656000b
Ansar <azhdarov.ansar@gmail.com>
2025-06-11 10:00:02 +0200

    removed keycloak

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,53 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
-import { includeBearerTokenInterceptor } from 'keycloak-angular';
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import { routes } from "./app.routes";
-
-import {
-  provideKeycloak,
-  createInterceptorCondition,
-  IncludeBearerTokenCondition,
-  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-  withAutoRefreshToken,
-  AutoRefreshTokenService,
-  UserActivityService
-} from 'keycloak-angular';
-
-const localhostCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
-  urlPattern: /^(http:\/\/localhost:8080)(\/.*)?$/i
-});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
-    provideKeycloak({
-      config: {
-        realm: "innovactions",
-        url: "http://localhost:8081",
-        clientId: "angular"
-      },
-      initOptions: {
-        onLoad: 'check-sso',
-        silentCheckSsoRedirectUri: location.origin + "/silent-check-sso.html",
-        redirectUri: location.origin + "/"
-      },
-      features: [
-        withAutoRefreshToken({
-           onInactivityTimeout: 'logout',
-           sessionTimeout: 60000
-         })
-      ],
-      providers: [
-        AutoRefreshTokenService,
-        UserActivityService,
-        {
-          provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-          useValue: [localhostCondition]
-        }
-      ]
-    })
+    provideHttpClient(withFetch()),
   ]
 };
